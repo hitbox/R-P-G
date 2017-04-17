26e09cc4aa638741f577de65f0b5ae45081522b8
Carl Harris <elgoogemail2007@gmail.com>
2017-04-17 13:03:39 -0400

    DEV: playing around

--- core2.py
+++ core2.py
@@ -14,25 +14,6 @@ class Font(object):
         return self._font.size(text)
 
 
-def label(text, color, background, border, padding=(50, 25)):
-    font = Font()
-
-    rect = pygame.Rect((0,0), font.size(text))
-    rect.width += padding[0]
-    rect.height += padding[1]
-
-    image = pygame.Surface(rect.size)
-    image.fill(background)
-
-    pygame.draw.rect(image, border, rect, 1)
-
-    textimage = font.render(text, color)
-    textrect = textimage.get_rect()
-    textrect.center = rect.center
-
-    image.blit(textimage, textrect)
-    return image
-
 def rect_label(text, rect, color, background, border):
     font = Font()
 
@@ -50,7 +31,7 @@ def rect_label(text, rect, color, background, border):
 
 class Button(pygame.sprite.Sprite):
 
-    def __init__(self, text, rect, *groups):
+    def __init__(self, text, rect, on_click, *groups):
         pygame.sprite.Sprite.__init__(self, *groups)
 
         self.images = dict(
@@ -60,26 +41,48 @@ class Button(pygame.sprite.Sprite):
         self.image = self.images['normal']
         self.rect = self.image.get_rect()
 
-    def on_hover(self, event):
-        self.image = self.images['hovering']
+        self.on_click_callback = on_click
 
     def exit_hover(self, event):
         self.image = self.images['normal']
 
+    def on_click(self, event):
+        print(self, event, self.on_click_callback)
+        if callable(self.on_click_callback):
+            self.on_click_callback(event)
+
+    def on_hover(self, event):
+        self.image = self.images['hovering']
+
 
 class VerticalButtonGroup(pygame.sprite.OrderedUpdates):
 
-    def __init__(self, labels):
+    def __init__(self, labels_and_callbacks):
         font = Font()
-        sizes = map(font.size, labels)
+        labels = list(t[0] for t in labels_and_callbacks)
+        callbacks = list(t[1] for t in labels_and_callbacks)
+        sizes = list(map(font.size, labels))
         rect = pygame.Rect(0, 0, max(size[0] for size in sizes), max(size[1] for size in sizes))
-        rect.inflate_ip(50, 25)
+        rect.width += 50
+        rect.height += 25
 
-        buttons = [ Button(text, rect) for text in labels ]
+        buttons = [ Button(text, rect, callback) for text, callback in zip(labels, callbacks) ]
         for b1, b2 in zip(buttons[:-1], buttons[1:]):
             b2.rect.top = b1.rect.bottom
 
         pygame.sprite.OrderedUpdates.__init__(self, *buttons)
+
+    def center(self, position):
+        rect = pygame.Rect(min(button.rect.left for button in self),
+                           min(button.rect.top for button in self),
+                           max(button.rect.width for button in self),
+                           sum(button.rect.height for button in self))
+        rect2 = rect.copy()
+        rect2.center = position
+
+        dx, dy = rect2.x - rect.x, rect2.y - rect.y
+        for button in self:
+            button.rect.move_ip(dx, dy)
 
 
 def main():
@@ -93,8 +96,12 @@ def main():
     screen_rect = screen.get_rect()
     clock = pygame.time.Clock()
 
+    def quit(event):
+        pygame.event.post(pygame.event.Event(pygame.QUIT))
+
     hovering = None
-    buttons = VerticalButtonGroup(['Start', 'Info', 'Exit'])
+    buttons = VerticalButtonGroup([('Start', None), ('Info', None), ('Exit', quit)])
+    buttons.center(screen_rect.center)
 
     running = True
     while running:
@@ -104,7 +111,7 @@ def main():
 
             elif event.type == pygame.KEYDOWN:
                 if event.key in (pygame.K_q, pygame.K_ESCAPE):
-                    pygame.event.post(pygame.event.Event(pygame.QUIT))
+                    quit(event)
 
             elif event.type == pygame.MOUSEMOTION:
                 for button in buttons:
@@ -120,11 +127,14 @@ def main():
                         hovering.exit_hover(event)
                         hovering = None
 
+            elif event.type == pygame.MOUSEBUTTONUP:
+                for button in buttons:
+                    hit = button.rect.collidepoint(event.pos)
+                    if hit and event.button == 1:
+                        button.on_click(event)
 
         screen.fill((0,0,0))
-
         buttons.draw(screen)
-
         pygame.display.update()
 
         dt = clock.tick(FRAMERATE)
